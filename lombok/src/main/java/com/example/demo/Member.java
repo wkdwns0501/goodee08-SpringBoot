66ac13bada2abc1221756732c4e7b79db56097bc
wkdwns0501 <britain0501@naver.com>
2026-03-12 01:20:09 +0900

    Lombok 기반 회원 모델 추가

---
+++ lombok/src/main/java/com/example/demo/Member.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Member {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+
+}
