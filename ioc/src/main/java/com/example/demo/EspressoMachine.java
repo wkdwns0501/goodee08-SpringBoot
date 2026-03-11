3355b70ce23bfe8dc9a5d933f7b481a2393bf199
wkdwns0501 <britain0501@naver.com>
2026-03-12 01:20:04 +0900

    에스프레소 머신 구현 추가

---
+++ ioc/src/main/java/com/example/demo/EspressoMachine.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+//@Component
+@Component("espressoMachine")
+public class EspressoMachine implements CoffeeMachine{
+    @Override
+    public String brew() {
+        return "Brewing coffee with Espresso Machine";
+    }
+}
