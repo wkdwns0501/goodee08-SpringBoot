bd5e2358293d8316cc30f541985340f9f48c88c4
wkdwns0501 <britain0501@naver.com>
2026-03-12 01:20:11 +0900

    회원 엔티티에 계정 상태 필드 추가

---
+++ restful-service/src/main/java/com/example/demo/entity/Member.java
@@ -0,0 +1,24 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private Integer age;
+    private String password;
+    private Boolean enabled;
+}
