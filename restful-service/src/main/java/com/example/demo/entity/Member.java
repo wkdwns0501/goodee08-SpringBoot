9c9f939bef7d850603732a931a8f6588190f00c3
wkdwns0501 <britain0501@naver.com>
2026-03-13 08:55:09 +0900

    회원과 게시글의 일대다 연관관계 및 삭제 전파 설정을 추가

--- restful-service/src/main/java/com/example/demo/entity/Member.java
+++ restful-service/src/main/java/com/example/demo/entity/Member.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Entity
 @Data
 @Builder
@@ -21,4 +23,10 @@ public class Member {
     private Integer age;
     private String password;
     private Boolean enabled;
+
+    // mappedBy: FK는 Article 엔티티의 member 속성이 관리한다는 의미(member 속성이 연관관계의 주인)
+    // cascade: 부모(Member)의 영속성 상태 변화가 자식(Article)에게 전파
+    // CascadeType.REMOVE: 부모가 삭제될 때 자식도 함께 삭제, 회원을 삭제할 때 해당 회원이 참조하는 게시글을 자동으로 삭제
+    @OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE)
+    private List<Article> articles;
 }
