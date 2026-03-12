85bee6d4c96254b0b36451e19ed77852b73b0c9a
wkdwns0501 <britain0501@naver.com>
2026-03-13 08:55:25 +0900

    조회 조건별 Query Method와 JPQL/Native Query 메서드를 추가

--- restful-service/src/main/java/com/example/demo/repository/MemberRepository.java
+++ restful-service/src/main/java/com/example/demo/repository/MemberRepository.java
@@ -2,8 +2,39 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    // 테스트 코드 작성을 위한 사전 준비
+    // JPA Query Methods
+    List<Member> findByName(String name);
+    List<Member> findByNameAndEmail(String name, String email);
+    List<Member> findByNameOrEmail(String name, String email);
+    List<Member> findByNameContaining(String name); // where name like %?%
+    List<Member> findByNameLike(String name); // where name like ?
+    List<Member> findByAgeGreaterThan(Integer age);
+    List<Member> findByAgeGreaterThanEqual(Integer age);
+    List<Member> findByAgeLessThan(Integer age);
+    List<Member> findByAgeLessThanEqual(Integer age);
+
+    // JPA Query Methods for ORDER BY
+    List<Member> findAllByOrderByNameAsc();
+
+    // JPA Query Methods for WHERE ORDER BY
+    List<Member> findByNameContainingOrderByNameAsc(String name);
+
+    // JPQL (Java Persistence Query Language)
+    @Query("SELECT m FROM Member m WHERE m.name = :name")
+    List<Member> findMemberByName(@Param("name") String name);
+    @Query("SELECT m FROM Member m WHERE m.name = :name AND m.email = :email")
+    List<Member> findMemberByNameEmail(@Param("name") String name, @Param("email") String email);
+    @Query("SELECT m FROM Member m WHERE m.enabled = :active AND m.age >= 19 AND m.email IS NOT NULL ORDER BY m.name")
+    List<Member> getActiveAdultWithEmail(@Param("active") boolean active);
+    @Query(value = "SELECT * FROM member WHERE enabled = :active AND age >= 19 AND email IS NOT NULL ORDER BY name", nativeQuery = true)
+    List<Member> getActiveAdultWithEmailByNative(@Param("active") boolean active);
 }
