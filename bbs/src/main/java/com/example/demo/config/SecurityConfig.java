2bc8fffad45836098b4be6dace0e260a84e7883d
wkdwns0501 <britain0501@naver.com>
2026-03-18 21:38:00 +0900

    메서드 단위 인가 처리를 위해 Spring Security 메서드 보안 설정을 활성화

--- bbs/src/main/java/com/example/demo/config/SecurityConfig.java
+++ bbs/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,6 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+@EnableMethodSecurity // 메소드 보안 활성화 설정
 @Configuration
 public class SecurityConfig {
 
